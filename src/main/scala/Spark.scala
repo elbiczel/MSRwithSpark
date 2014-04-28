44502cce26237ef06a13a82bedabfcbbb66aa75f
Tomasz A. Biczel <tomasz@biczel.com>
2014-04-28 21:39:17 +0200

    Spark class created

--- src/main/scala/Spark.scala
+++ src/main/scala/Spark.scala
@@ -1,15 +1,11 @@
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 
-object Spark extends GitProcessor {
+class Spark(sc: SparkContext) extends GitProcessor {
+
+  private val reducer: (AuthorStats, AuthorStats) => AuthorStats = (acc, value) => acc.add(value)
 
   def apply(url: String): Map[String, AuthorStats] = {
-
-    val sparkHome = "/Users/tomek/Development/spark-0.9.1" //"/root/spark"
-    val sparkUrl = "local"
-    val jarFile = "target/scala-2.10/loc-assembly-1.0.jar"
-
-    val sc = new SparkContext(sparkUrl, "loc", sparkHome, Seq(jarFile))
     val broadcastUrl = sc.broadcast(url)
     val tmpRepo = GitRepo(broadcastUrl.value)
     val shas = (tmpRepo.getCommits :+ "").reverse.sliding(2).toSeq
@@ -24,6 +20,16 @@ object Spark extends GitProcessor {
     }.reduceByKey(reducer)
     authStats.collectAsMap().toMap
   }
+}
 
-  private val reducer: (AuthorStats, AuthorStats) => AuthorStats = (acc, value) => acc.add(value)
+object Spark extends GitProcessor  {
+
+  val sparkHome = "/Users/tomek/Development/spark-0.9.1" //"/root/spark"
+  val sparkUrl = "local"
+  val jarFile = "target/scala-2.10/loc-assembly-1.0.jar"
+
+  val sc = new SparkContext(sparkUrl, "loc", sparkHome, Seq(jarFile))
+  val spark = new Spark(sc)
+
+  def apply(url: String): Map[String, AuthorStats] = spark(url)
 }
