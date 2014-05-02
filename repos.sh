36df778754d365ac4832e53b4d3cb7af535d1c96
Tomasz A. Biczel <tomasz@biczel.com>
2014-05-02 09:43:56 +0200

    Sbt and fetching repos scripts

---
+++ repos.sh
@@ -0,0 +1,4 @@
+git clone https://android.googlesource.com/platform/frameworks/volley # 1,681
+git clone https://github.com/spring-projects/spring-framework.git # 8,472
+git clone https://github.com/jenkinsci/jenkins.git # 18,424
+git clone https://github.com/apache/httpd.git # 26,031
